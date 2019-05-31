f66ae1f032d4dc681c874a0226002b8e310652c1
topoftheyear <topoftheyear@Gmail.com>
2019-05-31 00:17:18 -0500

    Finally attached the star pixel spawner to the camera instead of the player

--- Assets/Resources/Scripts/PixelSpawner.cs
+++ Assets/Resources/Scripts/PixelSpawner.cs
@@ -15,7 +15,7 @@ public class PixelSpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.Find("Player");
+        player = GameObject.Find("Main Camera");
     }
 
     // Update is called once per frame
