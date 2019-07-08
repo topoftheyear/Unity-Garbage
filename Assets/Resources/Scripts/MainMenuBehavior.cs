dfbac775b3a848f603301c1384ee24aa13d17ea8
topoftheyear <topoftheyear@Gmail.com>
2019-07-08 03:13:41 -0500

    Some menu work and scene loading rework

--- Assets/Resources/Scripts/MainMenuBehavior.cs
+++ Assets/Resources/Scripts/MainMenuBehavior.cs
@@ -2,16 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MainMenuBehavior : MonoBehaviour
 {
+    public CanvasGroup main, load;
+
     public Button exitButton, levelButton;
+
+    public Text loadText;
 
     // Start is called before the first frame update
     void Start()
     {
         exitButton.onClick.AddListener(ExitPress);
         levelButton.onClick.AddListener(LevelPress);
+
+        HideCanvas(load);
     }
 
 
@@ -22,6 +29,33 @@ public class MainMenuBehavior : MonoBehaviour
 
     public void LevelPress()
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene(2); // SlimeLevel
+        StartCoroutine(AsyncLoadScene("SlimeLevel"));
+    }
+
+    IEnumerator AsyncLoadScene(string name)
+    {
+        HideCanvas(main);
+        ShowCanvas(load);
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(name);
+        int x = 0;
+        while (!asyncLoad.isDone)
+        {
+            x++;
+            loadText.text += x + " ";
+            yield return null;
+        }
+    }
+
+    private void HideCanvas(CanvasGroup can)
+    {
+        can.alpha = 0f;
+        can.blocksRaycasts = false;
+    }
+
+    private void ShowCanvas(CanvasGroup can)
+    {
+        can.alpha = 1f;
+        can.blocksRaycasts = true;
     }
 }
