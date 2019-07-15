dd4cf56ef214c10bcca0d80d1bd9ced2bbf93e01
topoftheyear <topoftheyear@Gmail.com>
2019-07-15 03:48:21 -0500

    Added new enemy: splop

---
+++ Assets/Resources/Scripts/Enemies/Splop.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Splop : Enemy
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        base.Start();
+        health = 4;
+        max_health = health;
+
+        speed = 0.02f;
+    }
+
+    // Update is called once per frame
+    public override void Move()
+    {
+        this.transform.position += new Vector3(-speed, 0);
+    }
+}
